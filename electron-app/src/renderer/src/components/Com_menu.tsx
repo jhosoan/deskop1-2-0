cb35a088b8c418afbde6575d81fd4e965cbb0f95
jhosoan <jhosoancordeiro17@gmail.com>
2025-07-01 22:11:26 -0300

    melhorando menu

--- electron-app/src/renderer/src/components/Com_menu.tsx
+++ electron-app/src/renderer/src/components/Com_menu.tsx
@@ -6,9 +6,9 @@ export function Com_menu(){
         <>
           
           <div className="flex flex-row">
-              <Link to="/Tradicional">Tradicional</Link>;
-              <div className="basis-2xs">Reconhecido.</div>
-              
+              <Link className="duration-300 ease-in ... text-blue-500 m-5" to="/Tradicional">Tradicional</Link>
+              <Link className="duration-300 ease-in ... text-blue-500 m-5"  to="/Tradicional">Reconhecido.</Link>
+             
           </div>
         </>
     )
--- electron-app/src/renderer/src/pages/Menu.tsx
+++ electron-app/src/renderer/src/pages/Menu.tsx
@@ -2,9 +2,9 @@ import { Com_menu } from "@renderer/components/Com_menu"
 export function Menu(){
   return(
     <div>
-      <h1 className="text-amber-200">pagina de menuuuu</h1>
+      <h1 className="text-9x1">pagina de menu</h1>
+      <p className="ml-5">Escolha modalidade!</p>
       <Com_menu />
     </div>
-    
   )
 }
