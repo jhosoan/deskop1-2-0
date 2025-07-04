f1047993ba8823e84d9519b1b77df5e5b9406665
jhosoan <jhosoancordeiro17@gmail.com>
2025-07-04 15:46:50 -0400

    box head criado

--- electron-app/src/renderer/src/components/Com_Tradicio.tsx
+++ electron-app/src/renderer/src/components/Com_Tradicio.tsx
@@ -5,10 +5,12 @@ export function Com_Tradicio(){
         <header className="bg-blue-600 h-15 m-0">
           <h1 className=" ">box </h1>
           <div className="flex justify-evenly ...">
-                <div className=" bg-amber-400">01</div>
-                <div className=" bg-amber-400">02</div>
-                <div className=" bg-amber-400">03</div>
-                <div className=" bg-amber-400">04</div>
+                <div className="text-center bg-amber-400 w-10">
+                  <h1>#1</h1>
+                </div>
+                <div className=" bg-amber-400">br</div>
+                <div className="text-center bg-amber-400 w-290" >03</div>
+                <div className="text-center bg-red-600 w-13">1</div>
           </div>
           
         </header>
