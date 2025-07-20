38d8312939cbbd8741ed897ae15ff56e42bb75fb
jhosoan <jhosoancordeiro17@gmail.com>
2025-07-20 16:36:11 -0400

    95% da tela reconhecido criado

--- electron-app/src/renderer/src/components/Com_Tradicio.tsx
+++ electron-app/src/renderer/src/components/Com_Tradicio.tsx
@@ -20,17 +20,14 @@ export function Com_Tradicio(){
         <div className="bg-blue-900 h-90">
           <h1>bom 3</h1>
         </div>
-         <Link className="duration-300 ease-in ... text-blue-500 m-5" to="/">voltar</Link>
-
-
-         
+                
         <div className="bg-red-700 h-100">
           <div className="flex flex-wrap-reverse">
             <div>
-              <div className="truncate bg-white w-100 h-12 ">
-            <h1>00:00</h1>
+              <div className="truncate bg-white w-100 h-12 ml-6 mt-6">
+            <h1 className="text-center text-5xl">00:00</h1>
           </div>
-             <div>
+             <div className="bg-amber-400 ml-6">
                    <button className="m-10">START</button>
                    <button className="m-10" >STOP</button>
                    <button className="m-10">RESET</button>
@@ -38,11 +35,11 @@ export function Com_Tradicio(){
             </div>
             <div>               
             </div>
-            <div>
-               <div>
+            <div className="flex flex-wrap">
+               <div className="bg-amber-900 m-20 ">
                 <h1>POOMSAE 1</h1>
                </div>
-               <div>
+               <div className="bg-amber-600 m-20">
                 <h1>POOMSAE 2</h1>
                </div>
             </div>
